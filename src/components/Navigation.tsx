fd50e6304f5d864848cc43918a4da18183195166
viacheslav <kabaniets1990@gmail.com>
2025-02-04 10:29:06 +0200

    create blog app

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navigation = () => {
+    return (
+        <nav className='border-2 text-black border-gray-200 p-4 flex justify-between items-center'>
+            gwet
+        </nav>
+    )
+}
+
+export default Navigation
